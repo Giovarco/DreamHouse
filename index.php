d8a6af738205814079c977fcf6b604db4551445c
Unknown <kirby94@hotmail.it>
2018-03-24 22:20:09 +0100

    Index-php: - Mobile white gap fixed - Form is no longer centered

--- index.php
+++ index.php
@@ -11,61 +11,50 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
 		crossorigin="anonymous">
-		<link rel="stylesheet" href="css/custom_desktop.css"> <!-- css/custom.css -->
-		<link rel="stylesheet" href="css/custom_mobile.css"> <!-- css/custom.css -->
+		<link rel="stylesheet" href="css/custom.css">
+		<link rel="stylesheet" href="css/custom_mobile.css">
 
 		<title>Hello, world!</title>
 
 	</head>
 
-	<body class="h-100">
+	<body class="text-center h-100">
 
 		<!-- Header -->
 		<?php require 'php/common/header.php';?>
 
-		<main class="h-100 marginTop100">
+		<div class="container">
 
-			<div class="container h-100">
-
-				<div class="row h-100 centerContentOnPage">
-					<div class="col">
-
-						<!-- Title -->
-						<div class="row text-center text-white">
-							<div class="w-100">
-								<h1 id="title" >Let's Find You a Great Apartment.</h1>
-							</div>
-						</div>
-
-						<!-- Subtitle -->
-						<div class="row text-center text-white">
-							<div class="w-100">
-								<h2 id="subtitle">
-									<small>Join more than 5,828,476 happy renters searching 1,025,792 local apartments.</small>
-								</h2>
-							</div>
-						</div>
-
-						<!-- Search box -->
-						<div class="row w-100 align-items-center ml-0">
-							<div id="search" class="enterALocationSearchBox">
-								<input type="search" onchange="javascript:Home.confirmSelection();" class="text-center rounded w-100" list="cities" placeholder="Enter a Location here">
-								<datalist id="cities">
-									<option value="Milan">
-									<option value="Rome">
-								</datalist>
-							</div>
-						</div>
-
-
-					</div>
+			<!-- Title -->
+			<div class="row marginTop150px">
+				<div class="col">
+					<h1 id="title" class="text-white">Let's Find You a Great Apartment.</h1>
 				</div>
-
 			</div>
 
+			<!-- Subtitle -->
+			<div class="row">
+				<div class="col">
+					<h2 id="subtitle" class="text-white">
+						<small>Join more than 5,828,476 happy renters searching 1,025,792 local apartments.</small>
+					</h2>
+				</div>
+			</div>
 
-
-		</main>
+			<!-- Search box -->
+			<div class="row">
+				<div class="col">
+					<div id="search" class="enterALocationSearchBox align-items-center">
+						<input type="search" onchange="javascript:Home.confirmSelection();" class="text-center rounded w-100" list="cities" placeholder="Enter a Location here">
+						<datalist id="cities">
+							<option value="Milan">
+							<option value="Rome">
+						</datalist>
+					</div>
+				</div>
+			</div>
+			
+		</div>
 
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
